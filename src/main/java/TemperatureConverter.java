5c593c3830fc766547793384a9aee48f16c30c08
R.For_Swan <rfaure@et.esiea.fr>
2026-02-03 22:56:04 +0200

    SEP1 commit

---
+++ src/main/java/TemperatureConverter.java
@@ -0,0 +1,19 @@
+public class TemperatureConverter
+{
+    public double fahrenheitToCelsius(double tempFahrenheit)
+    {
+        return (tempFahrenheit - 32) * 5 / 9;
+    }
+
+    public double celsiusToFahrenheit(double tempCelsius)
+    {
+        return tempCelsius * 9 / 5 + 32;
+    }
+
+    public boolean isExtremeTemperature(double tempCelsius)
+    {
+        return tempCelsius < -40 || tempCelsius > 50;
+    }
+
+}
+
---
+++ src/test/java/TemperatureConverterTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TemperatureConverterTest
+{
+    TemperatureConverter temperatureConverter = new TemperatureConverter();
+
+    @DisplayName("fahrenheitToCelsius")
+    @Test
+    void fahrenheitToCelsius()
+    {
+        assertEquals(5.00d, temperatureConverter.fahrenheitToCelsius(41.00d));
+        assertEquals(0.00d, temperatureConverter.fahrenheitToCelsius(32.00d));
+        assertEquals(-15.00d, temperatureConverter.fahrenheitToCelsius(5.00d));
+    }
+
+    @DisplayName("celsiusToFahrenheit")
+    @Test
+    void celsiusToFahrenheit()
+    {
+        assertEquals(41.00d, temperatureConverter.celsiusToFahrenheit(5.00d));
+        assertEquals(32.00d, temperatureConverter.celsiusToFahrenheit(0.00d));
+        assertEquals(-31.00d, temperatureConverter.celsiusToFahrenheit(-35.00d));
+    }
+
+    @DisplayName("isExtremeTemperature")
+    @Test
+    void isExtremeTemperature() {
+        assertTrue(temperatureConverter.isExtremeTemperature(-41.0));
+        assertTrue(temperatureConverter.isExtremeTemperature(51.0));
+        assertFalse(temperatureConverter.isExtremeTemperature(-20.0));
+        assertFalse(temperatureConverter.isExtremeTemperature(20.0));
+        assertFalse(temperatureConverter.isExtremeTemperature(0.0));
+    }
+}
